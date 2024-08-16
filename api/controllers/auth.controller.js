be0231934e44abf7815f56a7215e52ab89ebd602
adarsh landge <adarsh.landge10604@gmail.com>
2024-08-16 10:34:01 +0530

    create sigin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import bcryptjs from 'bcryptjs';
 import User from './../models/user.model.js';
+import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 
 export const signUp=async(req,res,next)=>{
 
@@ -14,3 +16,25 @@ export const signUp=async(req,res,next)=>{
             }
 
 }
+
+
+export const signin=async(req,res,next)=>{
+    const {email,password}=req.body;
+    try{
+        const user =await User.findOne({email});
+        if(!user) return next(errorHandler(404,'User not found'));
+        const isValidPassword = bcryptjs.compareSync(password,user.password);
+        if(!isValidPassword) return next(errorHandler(401,'Invalid password'));
+        //To authenticate user (add cookie inside browser)
+        const token=jwt.sign({id:user._id},process.env.JWT_SECRET);
+
+        //destructure
+        const {password:pass,...rest}=user._doc;
+
+        res.cookie('access_token',token,{httpOnly:true})   //httpOnly true : no other third party have access to our cookie
+            .status(200)
+            .json(rest);
+    }catch(err){
+        next(err);
+        }
+    }       
